8edac5082667549dc838ff1d4590334537968c96
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-05 14:52:22 +0000

    generate tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from the contents of the web_static folder
+"""
+
+from datetime import datetime
+from os.path import isdir
+from fabric.api import local
+
+
+def do_pack():
+	"""generates a .tgz archive from folder contents"""
+	try:
+		date = datetime.now().strftime("%Y%m%d%H%M%S")
+		if isdir('versions') is False:
+			local("mkdir versions")
+		filename = "versions/web_static_{}.tgz".format(date)
+		local("tar -cvzf {} web_static".format(filename))
+
+		return filename
+	except:
+		return None
