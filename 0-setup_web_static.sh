6c9b2f691fcc1da7e879a4c3095cc378326bd5b6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-05 11:02:53 +0000

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Prepare web server
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+echo "Test Page" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
