5e75810770f30777899ef52c85b178e78958b600
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-06 14:07:12 +0000

    delete out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -71,5 +71,5 @@ def do_clean(number=0):
         number += 1
 
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = /data/web_static/releases
+    path = '/data/web_static/releases'
     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
