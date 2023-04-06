40759c7a04c0a46bf19ec31ba847acc9c5d59f58
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-06 14:47:01 +0000

    delete out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,72 +1,64 @@
 #!/usr/bin/python3
 """
-1. Generates a .tgz archive from the contents of the web_static folder
-2. Deploy archive to web server
-3. Full deploy
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
 """
 
-
+from fabric.api import *
 from datetime import datetime
 from os.path import exists, isdir
-from fabric.api import local, put, run, env
-
-
 env.hosts = ['54.236.26.66', '54.159.23.136']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa.pub'
 
 
 def do_pack():
-    """generates a .tgz archive from folder contents"""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir('versions') is False:
-            local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(filename))
-        return filename
-    except Exception:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """Deploy archive to web server"""
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        path_split = archive_path.replace('/', ' ').replace('.', ' ').split()
-        archive_parent_dir = path_split[0]
-        archivename_no_gz = path_split[1]
-        archivename_gz = path_split[1] + '.' + path_split[2]
-        dest_folder = '/data/web_static/releases/{}/'.format(archivename_no_gz)
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(dest_folder))
-        run('tar -xzf /tmp/{} -C {}/'.format(archivename_gz, dest_folder))
-        run('rm /tmp/{}'.format(archivename_gz))
-        run('mv {}/web_static/* {}'.format(dest_folder, dest_folder))
-        run('rm -rf {}/web_static'.format(dest_folder))
-        current = '/data/web_static/current'
-        run('rm -rf {}'.format(current))
-        run('ln -s {}/ {}'.format(dest_folder, current))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
 
 
 def deploy():
-    """Full deploy web content"""
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
-    if archive is None:
+    if archive_path is None:
         return False
     return do_deploy(archive_path)
 
 
 def do_clean(number=0):
-    """deletes out-of-date archives"""
+    """ CLEANS """
+
     number = int(number)
 
-    if number in (0, 1):
-        number = 1
+    if number == 0:
+        number = 2
     else:
         number += 1
 
