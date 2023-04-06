7e68527118ed6a5275f0933c35541b03fd8629fd
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-06 14:16:21 +0000

    do deploy webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,41 +13,42 @@ env.hosts = ['54.236.26.66', '54.159.23.136']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa.pub'
 
-'''
-def do_pack():
-	"""generates a .tgz archive from folder contents"""
-	try:
-		date = datetime.now().strftime("%Y%m%d%H%M%S")
-		if isdir('versions') is False:
-			local("mkdir -p versions")
-		filename = "versions/web_static_{}.tgz".format(date)
-		local("tar -cvzf {} web_static".format(filename))
 
-		return filename
-	except Exception:
-		return None
-'''
+def do_pack():
+    """ function generates a tgz archive from the contents of
+    the web_static folder of the AirBnB clone
+    """
+    try:
+        my_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        local("mkdir -p versions")
+        my_file = 'versions/web_static_' + my_time + '.tgz'
+        local('tar -vzcf {} web_static'.format(my_file))
+        return (my_file)
+    except Exception:
+        return None
+
 
 def do_deploy(archive_path):
-	"""Deploy archive to web server"""
-	if exists(archive_path) is False:
-		return False
-	try:
-		archive_path_split = archive_path.replace('/', ' ').replace('.', ' ').split()
-		archive_parent_dir = archive_path_split[0]
-		archivename_no_gz = archive_path_split[1]
-		archivename_gz = archive_path_split[1] + '.' + archive_path_split[2]
-		dest_folder = '/data/web_static/releases/{}/'.format(archivename_no_gz)
-		put(archive_path, '/tmp/')
-		run('mkdir -p {}'.format(dest_folder))
-		run('tar -xzf /tmp/{} -C {}/'.format(archivename_gz, dest_folder))
-		run('rm /tmp/{}'.format(archivename_gz))
-		run('mv {}/web_static/* {}'.format(dest_folder, dest_folder))
-		run('rm -rf {}/web_static'.format(dest_folder))
-		current = '/data/web_static/current'
-		run('rm -rf {}'.format(current))
-		run('ln -s {}/ {}'.format(dest_folder, current))
-
-		return True
-	except Exception:
-		return False
+    """ function distrubtes an archive to my web servers
+    """
+    path_existence = os.path.exists(archive_path)
+    if path_existence is False:
+        return False
+    try:
+        path_split = archive_path.replace('/', ' ').replace('.', ' ').split()
+        just_directory = path_split[0]
+        no_tgz_name = path_split[1]
+        full_filename = path_split[1] + '.' + path_split[2]
+        folder = '/data/web_static/releases/{}/'.format(no_tgz_name)
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(folder))
+        run('tar -xzf /tmp/{} -C {}/'.format(full_filename, folder))
+        run('rm /tmp/{}'.format(full_filename))
+        run('mv {}/web_static/* {}'.format(folder, folder))
+        run('rm -rf {}/web_static'.format(folder))
+        current = '/data/web_static/current'
+        run('rm -rf {}'.format(current))
+        run('ln -s {}/ {}'.format(folder, current))
+        return True
+    except Exception:
+        return False
