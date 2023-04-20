889ff84c18107d2318d8fc5cf87873ee7bf7b0f6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-20 17:49:28 +0000

    0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application, listening on 0.0.0.0, port 5000
+Routes:
+    /: display "Hello HBNB!"
+Use option strict_slashes=False in route definition
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays welcome message"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
