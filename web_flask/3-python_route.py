6255a9a6749ad4e28555716a27c0c23aca084a2d
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-21 12:02:21 +0200

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,21 +22,25 @@ def index():
     """displays welcome message"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun():
+def cisfun(text):
     """display c plus custom text"""
     return 'c ' + text.replace('_', ' ')
 
-@app.route('/python', strict_slashes=False)
+
+#@app.route('/python', strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """displays python is + custom text"""
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
