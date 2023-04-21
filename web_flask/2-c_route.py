1f6538032d2f47f5ef76c3b67bca0ff0d5e05801
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-21 11:46:12 +0200

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,15 +19,18 @@ def index():
     """displays welcome message"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun():
     """display c plus custom text"""
     return 'c ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
