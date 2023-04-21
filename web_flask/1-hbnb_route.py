0e00d19878ad07fdb2e078d40d2561bf9c787e8e
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-21 11:36:38 +0200

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,10 +17,12 @@ def index():
     """displays welcome message"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """displays HBNB"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
